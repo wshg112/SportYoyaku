a21c8504657f8ead26762e6606bc2878dff91691
王曙光 <wshg1982@hotmail.com>
2021-10-03 11:32:41 +0900

    エラー処理

--- Sports/Program.cs
+++ Sports/Program.cs
@@ -3,6 +3,7 @@ using OpenQA.Selenium;
 
 using OpenQA.Selenium.Support.UI;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 
@@ -13,33 +14,62 @@ namespace Sports
   
         static void Main(string[] args)
         {
+          
             var startTime=int.Parse(DateTime.Now.ToString("HH"));
             if (startTime >= 0 && startTime < 8)
             {
                 return;
             }
-
+            IWebDriver driver=null;
             // Chrome
             //var chromeOptions = new ChromeOptions();
             //chromeOptions.PageLoadStrategy = PageLoadStrategy.Default;
             //IWebDriver driver = new EdgeDriver();
 
             //Edge
-            var options = new EdgeOptions();
-            options.UseChromium = true;
-            var driver = new EdgeDriver(options);
+            
             try
             {
+                var options = new EdgeOptions();
+                options.UseChromium = true;
+
+                driver = new EdgeDriver(options);
                 new Reservation().SearchGoogle(driver);
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.ToString());
-                 new Reservation().sendLine("例外発生しました。"+ex.ToString());
+                var errmsg = "例外発生しました。" + ex.ToString();
+                try
+                {
+                    //msedgedriver
+                    var ps =
+                        System.Diagnostics.Process.GetProcessesByName("msedgedriver").ToList<System.Diagnostics.Process>();
+                    //msedgeのプロセスを取得
+                    ps.AddRange(System.Diagnostics.Process.GetProcessesByName("msedge").ToList<System.Diagnostics.Process>());
+
+
+                    foreach (System.Diagnostics.Process p in ps)
+                    {
+
+                        //プロセスを強制的に終了させる
+                        p.Kill();
+                    }
+                }
+                catch(Exception exps)
+                {
+                    errmsg = errmsg+ exps.ToString();
+
+                }
+                Console.WriteLine(errmsg);
+                new Reservation().sendLine(errmsg);
             }
             finally
             {
-                driver.Quit();               
+                if (driver != null)
+                {
+                    driver.Quit();
+                }
+                             
             }
 
 
