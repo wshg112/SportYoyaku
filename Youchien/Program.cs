9112581925e60dff2d4494ed3359d1ac8b9d3922
王曙光 <wshg1982@hotmail.com>
2022-09-17 00:13:22 +0900

    機能追加

---
+++ Youchien/Program.cs
@@ -0,0 +1,96 @@
+﻿//using Microsoft.Edge.SeleniumTools;
+using OpenQA.Selenium;
+
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using OpenQA.Selenium.Edge;
+
+namespace Yochien
+{
+    class Program
+    {
+  
+        static void Main(string[] args)
+        {
+
+            var arg = "";
+            if (args.Count() > 0) { 
+                arg=args[0];
+            }
+
+            if (arg != "AM" && arg != "PM" )
+            {
+                return;
+            }
+            IWebDriver driver=null;
+            // Chrome
+            //var chromeOptions = new ChromeOptions();
+            //chromeOptions.PageLoadStrategy = PageLoadStrategy.Default;
+            //IWebDriver driver = new EdgeDriver();
+
+            //Edge
+            var reservation = new Reservation(arg);
+            try
+            {
+                var options = new EdgeOptions();
+                //options.UseChromium = true;
+
+                driver = new EdgeDriver(options);
+                // = new EdgeDriver(new EdgeDriverService() , options, TimeSpan.FromSeconds(180));
+                //var service = EdgeDriverService.CreateDefaultService();
+                //service.HideCommandPromptWindow = true;
+                //EventFiringWebDriver driver = new EventFiringWebDriver(new EdgeDriver(service));
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMinutes(3);
+                reservation.SearchGoogle(driver);
+            }
+            catch (Exception ex)
+            {
+                var errmsg = "例外発生しました。" + ex.ToString();
+                try
+                {
+                    //msedgedriver
+                    var ps =
+                        System.Diagnostics.Process.GetProcessesByName("msedgedriver").ToList<System.Diagnostics.Process>();
+                    //msedgeのプロセスを取得
+                    ps.AddRange(System.Diagnostics.Process.GetProcessesByName("msedge").ToList<System.Diagnostics.Process>());
+
+
+                    foreach (System.Diagnostics.Process p in ps)
+                    {
+
+                        //プロセスを強制的に終了させる
+                        p.Kill();
+                    }
+                }
+                catch (Exception exps)
+                {
+                    errmsg = errmsg + exps.ToString();
+
+                }
+                Console.WriteLine(errmsg);
+                //PublishhMessage(errmsg);
+            }
+            finally
+            {
+                if (driver != null)
+                {
+                    driver.Quit();
+                }
+
+            }
+
+
+            //// InternetExplorer
+            //IWebDriver ie = new InternetExplorerDriver();
+            //SearchGoogle(ie);
+
+            //// FireFox
+            //FirefoxBinary firefoxBinary = new FirefoxBinary(@"C:\Program Files (x86)\Mozilla Firefox\firefox.exe");
+            //FirefoxProfile firefoxProfile = new FirefoxProfile();
+            //IWebDriver firefox = new FirefoxDriver(firefoxBinary, firefoxProfile);
+            //SearchGoogle(firefox);
+        }
+    }
+}
---
+++ Youchien/Reservation.cs
@@ -0,0 +1,137 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Yochien
+{
+    public class Reservation
+    {
+        String RunTime = "AM"; 
+        public Reservation(string time)
+        {
+            this.RunTime = time;
+        }
+        /// <summary>
+        /// 祝日リスト
+        /// </summary>
+        private string holidays;
+    
+        /// <summary>
+        /// 一宮市スポーツ予約システム
+        /// </summary>
+        /// <param name="driver"></param>
+        public void SearchGoogle(IWebDriver driver)
+        {
+            holidays = System.Configuration.ConfigurationManager.AppSettings["holidy"];
+
+            driver.Url = "https://www.kidsinfo.jp/";
+            IWait<IWebDriver> wait = new OpenQA.Selenium.Support.UI.WebDriverWait(driver, TimeSpan.FromSeconds(30.00));
+            wait.Until(driver1 => ((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState").Equals("complete"));
+            //IWebElement id_login = driver.FindElement(By.Name("id_login"));
+
+            //IWebElement pass_login = driver.FindElement(By.Name("pass_login"));
+
+            //id_login.SendKeys("uciu3848");
+            //pass_login.SendKeys("sm3DSD");
+            var inputs = driver.FindElements(By.TagName("input"));
+            foreach (var item in inputs)
+            {
+                if (item.GetAttribute("name")== "id_login")
+                {
+                    item.SendKeys("uciu3848");
+                }else if (item.GetAttribute("name") == "pass_login")
+                {
+                    item.SendKeys("sm3DSD");
+                }else if (item.GetAttribute("value") ==  "利用開始" )
+                {
+                    item.Click();
+                    break;
+                }
+            }
+            var links = driver.FindElements(By.TagName("a"));
+            foreach (var item in links)
+            {
+                if (item.Text.Contains("バス位置確認"))
+                {
+                    item.Click();
+                    break;
+                }
+              
+            }
+
+            var busroots = driver.FindElements(By.TagName("a"));
+            foreach (var item in busroots)
+            {
+                if (RunTime == "AM")
+                {
+                    if (item.GetAttribute("href").Contains("type=go"))
+                    {
+                        item.Click();
+                        break;
+                    }
+                }else if(RunTime == "PM")
+                {
+                    if (item.GetAttribute("href").Contains("type=back"))
+                    {
+                        item.Click();
+                        break;
+                    }
+                }
+           
+            }
+            
+            var formhtml = driver.FindElement(By.Id("all")).GetAttribute("innerHTML");
+            var brs= formhtml.Split("<br>");
+            foreach (var item in brs)
+            {
+                if (item.Contains("走行しています"))
+                {
+                    PublishhMessage(item);
+                }
+               
+                
+               
+            }
+
+
+            //次のページ
+       
+            
+
+        }
+        /// <summary>
+        /// line送信
+        /// </summary>
+        /// <param name="message"></param>
+        public static async void PublishhMessage(string message)
+        {
+            // 発行したアクセストークン
+            var ACCESS_TOKEN = System.Configuration.ConfigurationManager.AppSettings["token"];
+
+            using (var client = new HttpClient())
+            {
+                // 通知するメッセージ
+                var content = new FormUrlEncodedContent(new Dictionary<string, string>
+                {
+                    { "message", message },
+                });
+
+                // ヘッダーにアクセストークンを追加
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ACCESS_TOKEN);
+
+                // 実行
+                var result = await client.PostAsync("https://notify-api.line.me/api/notify", content);
+            }
+        }
+
+
+    }
+}
